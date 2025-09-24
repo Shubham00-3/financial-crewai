94dfc8cf07f4038618ce8b54aa3555a566f6f98c
Shubham Gangwar <shubhamgangwar244@gmail.com>
2025-09-25 02:38:58 +0530

    minor changes

--- src/config.py
+++ src/config.py
@@ -1,7 +1,7 @@
 import os
 from dotenv import load_dotenv
-from crewai import LLM
+from langchain_ollama import ChatOllama
 
 load_dotenv()
 os.environ["SERPER_API_KEY"] = os.getenv("SERPER_API_KEY")
-LLM = LLM(model="gemini/gemini-1.5-flash", temperature=0.7, api_key=os.getenv("GOOGLE_API_KEY"))
+LLM = ChatOllama(model="ollama/deepseek-r1:7b", temperature=0.7)
